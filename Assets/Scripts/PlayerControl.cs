baa012109fc62fa32ff28adad6d0181309f13abe
NoahFeinberg <noahbfeinberg@gmail.com>
2019-03-13 20:13:34 -0500

    have menus and movement working for the plane just need to build in panel because honestly it sucks

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float thruster;
+    public float yaw;
+    public float pitch;
+    public float roll;
+    static float speed;
+    static float change;
+    static float limit;
+    public GameObject ship;
+    // Use this for initialization
+    void Start()
+    {
+        thruster = 0.0f;
+        speed = 0.5f;
+        change = 0.01f;
+        limit = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ship.transform.Rotate(pitch, yaw, roll, Space.Self);
+        ship.GetComponent<Rigidbody>().velocity = transform.forward * thruster;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            thruster += speed;
+            if (thruster > 150)
+                thruster = 150;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            thruster -= speed;
+            if (thruster < 0)
+                thruster = 0;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            pitch -= change;
+            if (pitch < -limit)
+                pitch = -limit;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pitch += change;
+            if (pitch > limit)
+                pitch = limit;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            roll += change;
+            if (roll > limit)
+                roll = limit;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            roll -= change;
+            if (roll < -limit)
+                roll = -limit;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            yaw += change;
+            if (yaw > limit)
+                yaw = limit;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            yaw -= change;
+            if (yaw < -limit)
+                yaw = -limit;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+        }
+
+
+
+
+
+    }
+}
+
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject start, tutorial, quit;
+    public int button_selected;
+    Color unselected, selected;
+    // Use this for initialization
+    void Start()
+    {
+        button_selected = 0;
+        unselected = new Color(0.20392156862f, 0.65882352941f, 0.32549019607f);//green
+        selected = new Color(0.98431372549f, 0.73725490196f, 0.01960784313f); //yellow
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //state 0 is start menu
+            selection();
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                    Application.Quit();
+#endif
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                button_selected = (button_selected + 1) % 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                button_selected = (button_selected + 1) % 3;
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                button_selected = (button_selected + 2) % 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                button_selected = (button_selected + 2) %3;
+            }
+            //selection();
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if(button_selected == 2)
+                {
+                    #if UNITY_EDITOR
+                        UnityEditor.EditorApplication.isPlaying = false;
+                    #else
+                        Application.Quit();
+                    #endif
+                }
+                else
+                {
+                    SceneManager.LoadScene("Enviroment", LoadSceneMode.Single);
+                }
+            }   
+
+    }
+
+    void selection()
+    {
+        if (button_selected == 0)
+        {
+            start.GetComponent<Renderer>().material.color = selected;
+        }
+        else
+        {
+            start.GetComponent<Renderer>().material.color = unselected;
+        }
+
+        if (button_selected == 1)
+        {
+            tutorial.GetComponent<Renderer>().material.color = selected;
+        }
+        else
+        {
+            tutorial.GetComponent<Renderer>().material.color = unselected;
+        }
+
+        if (button_selected == 2)
+        {
+            quit.GetComponent<Renderer>().material.color = selected;
+        }
+        else
+        {
+            quit.GetComponent<Renderer>().material.color = unselected;
+        }
+    }
+}
